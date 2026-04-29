28039a6ed275633e643fa55563c99fda195e4c8d
Onno Kleen <onnokleen@users.noreply.github.com>
2026-04-29 11:35:02 +0200

    new default

--- scripts/download_spy_taq_wrds.R
+++ scripts/download_spy_taq_wrds.R
@@ -41,7 +41,7 @@ parse_args <- function(args) {
   defaults <- list(
     symbol = "SPY",
     start = "2015-01-01",
-    end = "2025-12-31",
+    end = "2024-12-31",
     out = "data/taq_spy",
     user = Sys.getenv("WRDS_USER", unset = ""),
     exchanges = "N,T,Q,A,P",
